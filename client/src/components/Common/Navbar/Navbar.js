f51eb197d0c68d1a258f632e157733e6d4a2bebf
kraskoo <kraskoo87@gmail.com>
2019-03-07 01:50:34 +0200

    Add bootstrap and react-bootstrap packages

--- client/src/components/Common/Navbar/Navbar.js
+++ client/src/components/Common/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Navbar as NavBar, Nav, NavDropdown } from 'react-bootstrap';
 import SessionService from '../../../services/session';
 import './Navbar.css';
 
@@ -7,6 +8,7 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.handleLogout = this.handleLogout.bind(this);
+    this.onClickListItem = this.onClickListItem.bind(this);
   }
 
   handleLogout(e) {
@@ -31,66 +33,59 @@ class Navbar extends Component {
       this.props.toast.error('jwtString or email is missing');
     }
   }
+  
+  // This function fix issue of incorrect behavior of NavDropdown
+  onClickListItem() {
+    const anchor = document.getElementById('user-dropdown');
+    const dropdown = anchor.parentNode;
+    anchor.setAttribute('aria-expanded', false);
+    dropdown.classList.remove('open');
+    anchor.click();
+  }
 
   render() {
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapsed" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
+      <NavBar>
+        <NavBar.Header>
+          <NavBar.Brand>
             <NavLink to="/" className="navbar-brand">Home</NavLink>
-          </div>
-          <div className="collapse navbar-collapse" id="navbar-collapsed">
-            <ul className="nav navbar-nav">
-              {
-                this.props.user ?
-                  <Fragment>
-                    <li><NavLink to="/tag/all">All Tags</NavLink></li>
-                    <li><NavLink to="/category/all">All Category</NavLink></li>
-                  </Fragment> :
-                  null
-              }
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              {
-                this.props.user ?
-                  (
-                    <Fragment>
-                      <ul className="nav navbar-nav">
-                        <li className="dropdown">
-                          <span className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hello, {this.props.user.username}!<span className="caret"></span></span>
-                          <ul className="dropdown-menu">
-                            {
-                              this.props.user.roles.includes('Admin') ?
-                              <Fragment>
-                                <li><NavLink to="/user/setadminrole">Set Admin Role</NavLink></li>
-                                <li><NavLink to="/tag/create">Create Tag</NavLink></li>
-                                <li><NavLink to="/category/create">Create Category</NavLink></li>
-                                <li role="separator" className="divider"></li>
-                              </Fragment> : null
-                            }
-                            <li><span id="logout" onClick={this.handleLogout}>Logout</span></li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </Fragment>
-                  ) :
-                  (
-                    <Fragment>
-                      <li><NavLink to="/user/register">Register</NavLink></li>
-                      <li><NavLink to="/user/login">Login</NavLink></li>
-                    </Fragment>
-                  )
-              }
-            </ul>
-          </div>
-        </div>
-      </nav>
+          </NavBar.Brand>
+          <NavBar.Toggle />
+        </NavBar.Header>
+        <NavBar.Collapse>
+          <Nav>
+            {
+              this.props.user ?
+                <Fragment>
+                  <li><NavLink to="/tag/all">All Tags</NavLink></li>
+                  <li><NavLink to="/category/all">All Category</NavLink></li>
+                </Fragment> :
+                null
+            }
+          </Nav>
+          <Nav pullRight>
+          {
+            this.props.user ?
+              <NavDropdown title={`Hello, ${this.props.user.username}!`} id="user-dropdown">
+                  {
+                    this.props.user.roles.includes('Admin') ?
+                      <Fragment>
+                        <li onClick={this.onClickListItem}><NavLink to="/user/setadminrole">Set Admin Role</NavLink></li>
+                        <li onClick={this.onClickListItem}><NavLink to="/tag/create">Create Tag</NavLink></li>
+                        <li onClick={this.onClickListItem}><NavLink to="/category/create">Create Category</NavLink></li>
+                        <li onClick={this.onClickListItem} role="separator" className="divider"></li>
+                      </Fragment> : null
+                  }
+                  <li><span id="logout" onClick={this.handleLogout}>Logout</span></li>
+              </NavDropdown> :
+              <Fragment>
+                <li><NavLink to="/user/register">Register</NavLink></li>
+                <li><NavLink to="/user/login">Login</NavLink></li>
+              </Fragment>
+          }
+          </Nav>
+        </NavBar.Collapse>
+      </NavBar>
     );
   }
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Carousel } from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
@@ -6,45 +7,26 @@ class Home extends Component {
     return (
       <div className="container">
         <h1>Home</h1>
-        <div id="carousel" className="carousel slide" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0" className="active"></li>
-            <li data-target="#carousel" data-slide-to="1"></li>
-            <li data-target="#carousel" data-slide-to="2"></li>
-            <li data-target="#carousel" data-slide-to="3"></li>
-            <li data-target="#carousel" data-slide-to="4"></li>
-            <li data-target="#carousel" data-slide-to="5"></li>
-          </ol>          
-          <div className="carousel-inner">
-            <div className="item active">
-              <img src="carousel/1.png" alt="Los Angeles" />
-            </div>
-            <div className="item">
-              <img src="carousel/2.png" alt="Chicago" />
-            </div>
-            <div className="item">
-              <img src="carousel/3.png" alt="New York" />
-            </div>
-            <div className="item">
-              <img src="carousel/4.png" alt="New York" />
-            </div>
-            <div className="item">
-              <img src="carousel/5.png" alt="New York" />
-            </div>
-            <div className="item">
-              <img src="carousel/6.png" alt="New York" />
-            </div>
-          </div>
-          
-          <a className="left carousel-control" href="#carousel" data-slide="prev">
-            <span className="glyphicon glyphicon-chevron-left"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="right carousel-control" href="#carousel" data-slide="next">
-            <span className="glyphicon glyphicon-chevron-right"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+        <Carousel>
+          <Carousel.Item>
+            <img src="carousel/1.png" alt="1" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel/2.png" alt="2" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel/3.png" alt="3" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel/4.png" alt="4" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel/5.png" alt="5" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel/6.png" alt="6" />
+          </Carousel.Item>
+        </Carousel>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem voluptatibus nobis, tenetur molestiae esse consectetur ullam sit perspiciatis eius maiores atque necessitatibus distinctio corrupti! Qui quaerat ratione animi dicta officiis?
 Non commodi et rem id. Incidunt dolores optio rem, qui temporibus neque laboriosam eaque! Obcaecati, quo ipsam! Dolores velit sed id nisi laboriosam accusantium quod rem deserunt, quaerat rerum facere.
 Facilis provident aspernatur fugiat placeat vel magni nostrum, alias sed. Praesentium earum quas enim quibusdam, pariatur, sequi ut molestiae aliquam ipsam maxime accusamus corrupti dicta architecto eveniet illo sunt molestias.
