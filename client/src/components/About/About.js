c387c42703e6f4b184828b4476b1350241279221
kraskoo <kraskoo87@gmail.com>
2019-03-12 19:48:56 +0200

    Update About Page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
-const mapsStyles = {
-  width: '600px',
-  height: '600px'
-};
+const mapsStyles = { width: '600px', height: '600px' };
+const smallStyles = { fontVariant: 'small-caps', display: 'block' };
 
 export default class About extends Component {
   componentDidMount() {
@@ -25,9 +23,11 @@ export default class About extends Component {
           This project is built by project assignment from the course <a href="https://softuni.bg/trainings/2282/reactjs-fundamentals-february-2019" rel="noopener noreferrer" target="_blank">ReactJS Fundamentals</a> at <a href="https://softuni.bg/" rel="noopener noreferrer" target="_blank">SoftUni</a>
         </h3>
         <br />
-        <h4>This project is built with no-commercial purpose</h4>
+        <h4>This project is built with non-commercial purpose</h4>
         <br />
-        <h4 className="text-center">this is just for demo</h4>
+        <h4 className="text-center">This map is just for demo!</h4>
+        <small className="text-center" style={smallStyles}>Probably it won't work correctly, this could be because of new billing system</small>
+        <br />
         <div id="map" className="col-centered" style={mapsStyles}></div>
       </div>
     );
