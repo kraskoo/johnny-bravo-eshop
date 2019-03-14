f1f2549d6b76f507237a24bea22757ce707ff407
kraskoo <kraskoo87@gmail.com>
2019-03-15 01:15:21 +0200

    1. Add 'react-responsive-carousel' package 2. Styling DeviceDetails component

--- client/src/components/Device/Details.js
+++ client/src/components/Device/Details.js
@@ -1,11 +1,28 @@
 import React, { Component, Fragment } from 'react';
 import DeviceService from '../../services/device';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Loading from '../Common/Loading';
 
-const imageStyle = {
-  maxWidth: '350px',
-  maxHeight: '350px',
-};
+const imageStyle = { maxWidth: '400px' };
+const rightAlign = { textAlign: 'right' };
+const italic = { fontStyle: 'italic' };
+
+const CarouselImages = (device) => (
+  <div className="col-md-8 col-centered">
+    <Carousel showIndicators={false} showStatus={false} autoPlay={true} emulateTouch={true} infiniteLoop={true}>
+      {device.imageUrls.map((img, i) => (
+        <div key>
+          <img src={img} key={`${device._id}image${i}`} style={imageStyle} alt={device.name} />
+        </div>
+      ))}
+    </Carousel>
+  </div>
+);
+
+const Characteristics = (device) => (
+  device.characteristics.map((c, i) => (<li key={`${device._id}characteristic${i}`}>{c}</li>))
+);
 
 class DeviceDetails extends Component {
   constructor(props) {
@@ -34,22 +51,31 @@ class DeviceDetails extends Component {
     const device = this.state.device;
     return (
       <div className="container">
-        <h1>Name: {device.name}</h1>
-        <h3>Description:</h3>
-        <p>{device.description}</p>
-        <h3>Characteristics:</h3>
-        <ul>
-          {device.characteristics.map((c, i) => (<li key={`${device._id}characteristic${i}`}>{c}</li>))}
-        </ul>
-        <p>Price: ${device.price}</p>
+        <h1>{device.name}</h1>
+        <p style={italic}>{device.description}</p>
+        <h4 style={rightAlign} className="text-info">Price: ${device.price}</h4>
         {
           this.props.user && this.props.user.roles.includes('Admin') ?
           <Fragment>
-            <small>Quantity: {device.quantity}</small> <br />
+            <p style={rightAlign} className="text-primary">Quantity: {device.quantity}</p>
           </Fragment> :
           null
         }
-        {device.imageUrls.map((img, i) => (<img src={img} key={`${device._id}image${i}`} style={imageStyle} alt={device.name} />))}
+        <ul className="nav nav-tabs">
+          <li className="active"><a data-toggle="tab" href="#images">Images</a></li>
+          <li><a data-toggle="tab" href="#characteristics">Characteristics</a></li>
+        </ul>
+        <div className="tab-content">
+          <div id="images" className="tab-pane fade in active">
+            {CarouselImages(device)}
+          </div>
+          <div id="characteristics" className="tab-pane fade">
+            <div className="col-md-8">
+              <br />
+              <ul>{Characteristics(device)}</ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
